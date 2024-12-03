c455aaa5a24d8d2007ce2f46ee7d92b20977204e
buhkor@gmail.com <buhkor@gmail.com>
2024-12-03 21:19:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pushka
+{
+    class Gun 
+    {
+        private bool isReoled;
+
+        private void Rolead()
+        {
+            Console.WriteLine("Заряжаю");
+            isReoled = true;
+            Console.WriteLine("Заряжено");
+        }
+
+        public void Shoot()
+        {
+            if (!isReoled)
+            {
+                Console.WriteLine("Орудие не заряжено");
+                Rolead();
+            }
+            Console.WriteLine("Пыщь - Пыщь!");
+            isReoled = false;
+
+        }
+    }    
+
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Gun gun = new Gun();
+            gun.Shoot();
+
+        }
+
+    }
+}
